37bea821a1d7465c8b31637d75adc55b94900581
Devin MacGillivray <devhmac@gmail.com>
2021-04-27 18:20:10 +0000

    adding links and form validation

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -7,12 +7,16 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
   def new
+    @article = Article.new
   end
   def create
     @article = Article.new(article_params)
    
-    @article.save
+    if @article.save
     redirect_to @article
+    else
+      render 'new'
+    end
   end
    
   private
